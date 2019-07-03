d331be14146576f74dfcee45c5829dea9f2e7255
Taha Muneeb <taha.muneeb@irsavideo.com>
2019-07-03 14:00:55 +0500

    Changed

--- CodeFirst/Models/ApplicationDbContext.cs
+++ CodeFirst/Models/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ using System.Web;
 
 namespace CodeFirst.Models
 {
+
+    //ye class khud banani ha uska bad DbContext se inherit krwao pher constructor banao uska uska bad usko base se relate krwao or wahan pe database ka name dedo jo k bad mein webconfig mein b same use hoga
     public class ApplicationDbContext: DbContext
     {
         public ApplicationDbContext() : base("DBCS")
@@ -14,7 +16,7 @@ namespace CodeFirst.Models
         }
 
 
-
+        //yahan pe jitne b database banain ge uska dbset aiga isi tarhan
         public DbSet<Student> Student { get; set; }
     }
 }
